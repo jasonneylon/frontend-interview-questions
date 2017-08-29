7c2040a3d26a42d46b1630bbb7414d4265810092
Jason Neylon <jason.neylon@gmail.com>
2017-08-29 09:14:33 +0200

    basic memo implementation of fib2

--- fib2/fib.js
+++ fib2/fib.js
@@ -1,9 +1,36 @@
+const memo = {};
+
 function fib(n) {
-  switch(n) {
-    case 0: return 0
-    case 1: return 1
-    default: return fib(n-1) + fib(n-2)
+  if (memo[n]) {
+    return memo[n];
   }
+  let returnValue;
+  switch(n) {
+    case 0: 
+      returnValue = 0;
+      break;
+    case 1: return 1
+      returnValue = 1;
+      break;
+    default: 
+      returnValue = fib(n-1) + fib(n-2);
+  }
+  memo[n] = returnValue;
+  return returnValue;
 }
+
+// function memo(fn) {
+//   const memory = {};
+
+//   return function() {
+//     if (memory[args[0]]) {
+//       return (args[0]);
+//     }
+//     const result = fn(args);
+//     memory[args[0]] = result;
+//     return result;
+//   }
+// }
+
 
 module.exports.fib = fib;
